c94c5ab4a88d1c1f16aeefb5c50f90db3e2fc190
= <=>
2024-10-31 23:26:11 +0100

    adding files

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,25 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/kishan.jpeg";
+import avatarImage from "../assets/mappollonos.jpeg";
 import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function About() {
   return (
     <div className="profile">
       <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
+      <h1 className="profile-text">Hi I am Adefela Shogbanmu</h1>
       <h2 className="profile-text">The creator of this awesome pokedex</h2>
       <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
+        This project is created for amusement and thrill!
       </h4>
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="https://github.com/Gabrielshow">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
+        <a href="https://www.youtube.com/c/">
           <FaYoutube />
         </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
+        <a href="https://www.linkedin.com/in/adefela-shogbanmu-594289258/">
           <FaLinkedin />
         </a>
       </div>
